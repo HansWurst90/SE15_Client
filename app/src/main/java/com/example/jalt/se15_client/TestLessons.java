5599461c0fce4471b3c85e2cbd346461657ec893
HansWurst90 <Jan@mussenbrock-w.de>
2015-06-10 15:15:27 +0200

    TestLessons angepasst

--- app/src/main/java/com/example/jalt/se15_client/TestLessons.java
+++ app/src/main/java/com/example/jalt/se15_client/TestLessons.java
@@ -16,27 +16,22 @@ import studeasy.entities.Teacher;
  * Created by ErfiMac on 10.06.15.
  */
 public class TestLessons {
-    private Date date;
-    private String subject;
-    private Teacher teacher;
-    private Room room;
-    private Lesson lesson1 = new Lesson();
-    private Lesson lesson2 = new Lesson();
-    private Lesson lesson3 = new Lesson();
-    private Lesson lesson4 = new Lesson();
-    private Lesson lesson5 = new Lesson();
-    private Lesson lesson6 = new Lesson();
 
-    private List<Lesson> lessons = new ArrayList<Lesson>();
+    static List<Lesson> lessons = new ArrayList<Lesson>();
 
-    public TestLessons()
+    static List<Lesson> getLessons(){
     {
-        create();
-        fill();
-    }
+         Date date;
+         String subject;
+         Teacher teacher;
+         Room room;
+         Lesson lesson1 = new Lesson();
+         Lesson lesson2 = new Lesson();
+         Lesson lesson3 = new Lesson();
+         Lesson lesson4 = new Lesson();
+         Lesson lesson5 = new Lesson();
+         Lesson lesson6 = new Lesson();
 
-    private void create()
-    {
         //Lesson 1
         lesson1.setDate(new Date());
             Subject subject1 = new Subject();
@@ -46,11 +41,11 @@ public class TestLessons {
             Teacher teacher1 = new Teacher();
             teacher1.setName("Mußenbrock");
             teacher1.setGender('m');
-        lesson1.setTeacher(teacher);
+        lesson1.setTeacher(teacher1);
         lesson1.setLessonHour(1);
             Room room1 = new Room();
             room1.setRoomID("C501");
-        lesson1.setRoom(room);
+        lesson1.setRoom(room1);
         lesson1.setCourse(new Course());
             Homework homework1 = new Homework();
             homework1.setDescription("English Homework");
@@ -65,11 +60,11 @@ public class TestLessons {
             Teacher teacher2 = new Teacher();
             teacher2.setName("Prischep");
             teacher2.setGender('m');
-        lesson2.setTeacher(teacher);
+        lesson2.setTeacher(teacher2);
         lesson2.setLessonHour(2);
             Room room2 = new Room();
             room2.setRoomID("A004");
-        lesson2.setRoom(room);
+        lesson2.setRoom(room2);
         lesson2.setCourse(new Course());
             Homework homework2 = new Homework();
             homework2.setDescription("Aufsatz verfassen");
@@ -84,11 +79,11 @@ public class TestLessons {
             Teacher teacher3 = new Teacher();
             teacher3.setName("Erfkämper");
             teacher3.setGender('m');
-        lesson3.setTeacher(teacher);
+        lesson3.setTeacher(teacher3);
         lesson3.setLessonHour(3);
             Room room3 = new Room();
             room3.setRoomID("D422");
-        lesson3.setRoom(room);
+        lesson3.setRoom(room3);
         lesson3.setCourse(new Course());
             Homework homework3 = new Homework();
             homework3.setDescription("Satz des Phythagoras nachweisen");
@@ -103,11 +98,11 @@ public class TestLessons {
             Teacher teacher4 = new Teacher();
             teacher4.setName("Riegel");
             teacher4.setGender('w');
-        lesson4.setTeacher(teacher);
+        lesson4.setTeacher(teacher4);
         lesson4.setLessonHour(4);
             Room room4 = new Room();
             room4.setRoomID("B201");
-        lesson4.setRoom(room);
+        lesson4.setRoom(room4);
         lesson4.setCourse(new Course());
             Homework homework4 = new Homework();
             homework4.setDescription("Handstand üben");
@@ -122,11 +117,11 @@ public class TestLessons {
             Teacher teacher5 = new Teacher();
             teacher5.setName("Einstein");
             teacher5.setGender('m');
-        lesson5.setTeacher(teacher);
+        lesson5.setTeacher(teacher5);
         lesson5.setLessonHour(5);
             Room room5 = new Room();
             room5.setRoomID("D601");
-        lesson5.setRoom(room);
+        lesson5.setRoom(room5);
         lesson5.setCourse(new Course());
             Homework homework5 = new Homework();
             homework5.setDescription("Aufsatz über die Relativitätstheorie verfassen");
@@ -141,25 +136,29 @@ public class TestLessons {
             Teacher teacher6 = new Teacher();
             teacher6.setName("von Neumann");
             teacher6.setGender('m');
-        lesson6.setTeacher(teacher);
+        lesson6.setTeacher(teacher6);
         lesson6.setLessonHour(6);
             Room room6 = new Room();
             room6.setRoomID("C333");
-        lesson6.setRoom(room);
+        lesson6.setRoom(room6);
         lesson6.setCourse(new Course());
             Homework homework6 = new Homework();
             homework6.setDescription("App programmieren");
         lesson6.setHomework(homework6);
-    }
 
-    private void fill()
-    {
         lessons.add(lesson1);
         lessons.add(lesson2);
         lessons.add(lesson3);
         lessons.add(lesson4);
         lessons.add(lesson5);
         lessons.add(lesson6);
+
+        return lessons;
+    }
+
+
+
+
 
     }
 }
