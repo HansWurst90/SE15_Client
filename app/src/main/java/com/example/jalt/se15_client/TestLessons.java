cf3db2863d6c7f64a2f4bbd7bbae104be84d92dd
HansWurst90 <Jan@mussenbrock-w.de>
2015-06-14 21:12:25 +0200

    Teststunden erweitert

--- app/src/main/java/com/example/jalt/se15_client/TestLessons.java
+++ app/src/main/java/com/example/jalt/se15_client/TestLessons.java
@@ -180,34 +180,34 @@ public class TestLessons {
 
     static List<LessonTO> getLessons1(){
         List<LessonTO> lessons = new ArrayList<LessonTO>();
-        lessons.add(createLesson("Mathematik", 1, "Mußenbrock", 'm', 1, "A123", 1));
-        lessons.add(createLesson("Deutsch", 2, "Mußenbrock", 'm', 2, "A123", 2));
+        lessons.add(createLesson("Mathe", 1, "Erfkämper", 'm', 1, "A123", 1));
+        lessons.add(createLesson("Deutsch", 2, "Prischep", 'm', 2, "A123", 2));
 
-        lessons.add(createLesson("Spanisch", 4, "Mußenbrock", 'm', 4, "A123", 3));
-        lessons.add(createLesson("Biologie", 8, "Mußenbrock", 'm', 5, "A123", 4));
-        lessons.add(createLesson("Informatik", 6, "Mußenbrock", 'm', 6, "A123", 5));
+        lessons.add(createLesson("Spanisch", 4, "Goy Ramos", 'm', 4, "A123", 3));
+        lessons.add(createLesson("Biologie", 8, "Darwin", 'm', 5, "A123", 4));
+        lessons.add(createLesson("Info", 6, "Turing", 'm', 6, "A123", 5));
         return lessons;
     }
 
     static List<LessonTO> getLessons2(){
         List<LessonTO> lessons = new ArrayList<LessonTO>();
         lessons.add(createLesson("Englisch", 3, "Mußenbrock", 'm', 1, "A123", 6));
-        lessons.add(createLesson("Mathematik", 1, "Erfkämper", 'm', 2, "A123", 7));
-        lessons.add(createLesson("Chemie", 7, "Mußenbrock", 'm', 3, "A123", 8));
+        lessons.add(createLesson("Mathe", 1, "Erfkämper", 'm', 2, "A123", 7));
+        lessons.add(createLesson("Chemie", 7, "Curie", 'f', 3, "A123", 8));
 
-        lessons.add(createLesson("Physik", 9, "Mußenbrock", 'm', 5, "A123", 9));
-        lessons.add(createLesson("Informatik", 6, "Mußenbrock", 'm', 6, "A123", 10));
+        lessons.add(createLesson("Physik", 9, "Einstein", 'm', 5, "A123", 9));
+        lessons.add(createLesson("Info", 6, "Turing", 'm', 6, "A123", 10));
         return lessons;
     }
 
     static List<LessonTO> getLessons3(){
         List<LessonTO> lessons = new ArrayList<LessonTO>();
-        lessons.add(createLesson("Chemie", 7, "Mußenbrock", 'm', 1, "A123", 11));
-        lessons.add(createLesson("Deutsch", 2, "Mußenbrock", 'm', 2, "A123", 12));
-        lessons.add(createLesson("Biologie", 8, "Mußenbrock", 'm', 3, "A123", 13));
-        lessons.add(createLesson("Sport", 5, "Mußenbrock", 'm', 4, "A123", 1));
+        lessons.add(createLesson("Chemie", 7, "Curie", 'm', 1, "A123", 11));
+        lessons.add(createLesson("Deutsch", 2, "Prischep", 'm', 2, "A123", 12));
+        lessons.add(createLesson("Biologie", 8, "Darwin", 'm', 3, "A123", 13));
+        lessons.add(createLesson("Sport", 5, "Riegel", 'm', 4, "A123", 1));
         lessons.add(createLesson("Englisch", 3, "Mußenbrock", 'm', 5, "A123", 14));
-        lessons.add(createLesson("Religion", 10, "Mußenbrock", 'm', 6, "A123", 15));
+        lessons.add(createLesson("Religion", 10, "Ratzinger", 'm', 6, "A123", 15));
         return lessons;
     }
 
@@ -216,19 +216,19 @@ public class TestLessons {
 
 
         lessons.add(createLesson("Englisch", 3, "Mußenbrock", 'm', 3, "A123", 16));
-        lessons.add(createLesson("Informatik", 6, "Mußenbrock", 'm', 4, "A123", 17));
-        lessons.add(createLesson("Spanisch", 4, "Mußenbrock", 'm', 5, "A123", 18));
+        lessons.add(createLesson("Info", 6, "Turing", 'm', 4, "A123", 17));
+        lessons.add(createLesson("Spanisch", 4, "Goy Ramos", 'm', 5, "A123", 18));
         lessons.add(createLesson("Physik", 9, "Mußenbrock", 'm', 6, "A123", 19));
         return lessons;
     }
 
     static List<LessonTO> getLessons5() {
         List<LessonTO> lessons = new ArrayList<LessonTO>();
-        lessons.add(createLesson("Biologie", 8, "Mußenbrock", 'm', 1, "A123", 20));
-        lessons.add(createLesson("Mathematik", 1, "Erfkämper", 'm', 2, "A123", 21));
-        lessons.add(createLesson("Deutsch", 2, "Erfkämper", 'm', 3, "A123", 22));
+        lessons.add(createLesson("Biologie", 8, "Darwin", 'm', 1, "A123", 20));
+        lessons.add(createLesson("Mathe", 1, "Erfkämper", 'm', 2, "A123", 21));
+        lessons.add(createLesson("Deutsch", 2, "Prischep", 'm', 3, "A123", 22));
         lessons.add(createLesson("Sport", 5, "Erfkämper", 'm', 4, "A123", 23));
-        lessons.add(createLesson("Religion", 10, "Mußenbrock", 'm', 5, "A123", 24));
+        lessons.add(createLesson("Religion", 10, "Ratzinger", 'm', 5, "A123", 24));
         return lessons;
     }
 
