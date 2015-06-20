3dcb0e707809ff1aeef4fc81e7ada775d4cbc9dd
HansWurst90 <Jan@mussenbrock-w.de>
2015-06-20 17:52:06 +0200

    getLessonById funktioniert fast

--- app/src/main/java/com/example/jalt/se15_client/Soap2Object.java
+++ app/src/main/java/com/example/jalt/se15_client/Soap2Object.java
@@ -1,11 +1,6 @@
 package com.example.jalt.se15_client;
 import common.*;
-import org.ksoap2.HeaderProperty;
-import org.ksoap2.SoapEnvelope;
-import org.ksoap2.SoapFault;
 import org.ksoap2.serialization.SoapObject;
-import org.ksoap2.serialization.SoapSerializationEnvelope;
-import org.ksoap2.transport.HttpTransportSE;
 /**
  * Created by Jan on 20.06.15.
  */
@@ -23,14 +18,14 @@ public class Soap2Object {
         // Subject abholen und "entpacken
         SoapObject SoapSubject = (SoapObject) SoapLesson.getProperty("subject");
         int subjectID = Integer.parseInt(SoapSubject.getPrimitivePropertySafelyAsString("subjectID"));
-        String subjectDescription = SoapSubject.getPrimitivePropertySafelyAsString("subjectDescription");
+        String subjectDescription = SoapSubject.getPrimitivePropertySafelyAsString("description");
         SubjectTO subject = new SubjectTO();
         subject.setSubjectID(subjectID);
         subject.setDescription(subjectDescription);
         // Lesson abholen und "entpacken
         String room = SoapLesson.getPrimitivePropertySafelyAsString("room");
         String date = SoapLesson.getPrimitivePropertySafelyAsString("date");
-        int lessonHour = Integer.parseInt(SoapLesson.getPrimitivePropertySafelyAsString("LessonHour"));
+        int lessonHour = Integer.parseInt(SoapLesson.getPrimitivePropertySafelyAsString("lessonHour"));
         int lessonID = Integer.parseInt(SoapLesson.getPrimitivePropertySafelyAsString("lessonID"));
         LessonTO lesson = new LessonTO();
         lesson.setLessonID(lessonID);
