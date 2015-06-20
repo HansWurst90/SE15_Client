cfe8920818e15f5d75f0dc427d1b06dbc9da5530
PiCookie <l.erfkaemper@gmx.de>
2015-06-20 14:06:21 +0200

    Name und Firstname wird beim einloggen mitgeschickt

--- app/src/main/java/com/example/jalt/se15_client/tasks/LoginTask.java
+++ app/src/main/java/com/example/jalt/se15_client/tasks/LoginTask.java
@@ -68,6 +68,8 @@ public class LoginTask extends AsyncTask<Object, Void, UserLoginResponse> {
             // Persistierung
             SavePreferences("USER", "" + personid);
             SavePreferences("PASSWORD", "******");
+            SavePreferences("NAME", name);
+            SavePreferences("FIRSTNAME", firstname);
             SavePreferences("SESSIONID", "" + result.getSessionID());
             //Toast anzeigen
             CharSequence text = "Willkommen User " + personid;
