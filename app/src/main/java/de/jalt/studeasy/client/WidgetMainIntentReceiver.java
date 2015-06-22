ff44b78824e4db559bdb3d40f12ff9ca0b51205f
PiCookie <l.erfkaemper@gmx.de>
2015-06-22 17:57:01 +0200

    Widget funktioniert

--- app/src/main/java/de/jalt/studeasy/client/WidgetMainIntentReceiver.java
+++ app/src/main/java/de/jalt/studeasy/client/WidgetMainIntentReceiver.java
@@ -4,6 +4,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.preference.PreferenceManager;
 import android.widget.RemoteViews;
 
 import com.jalt.studeasy.client.R;
@@ -35,13 +37,19 @@ public class WidgetMainIntentReceiver extends BroadcastReceiver {
      */
     private void updateWidgetPictureAndButtonListener(Context context) {
         RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_main);
-        sharedPreferences = context.getSharedPreferences(WidgetMainIntentReceiver.class + "",Context.MODE_WORLD_READABLE);
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
         remoteViews.setTextViewText(R.id.widgetRow1, sharedPreferences.getString("LESSONROW1", ""));
+        remoteViews.setInt(R.id.widgetRow1, "setBackgroundColor", android.graphics.Color.argb(120, 255, 0, 0));
         remoteViews.setTextViewText(R.id.widgetRow2, sharedPreferences.getString("LESSONROW2", ""));
+        remoteViews.setInt(R.id.widgetRow2, "setBackgroundColor", android.graphics.Color.argb(120, 0, 0, 255));
         remoteViews.setTextViewText(R.id.widgetRow3, sharedPreferences.getString("LESSONROW3", ""));
+        remoteViews.setInt(R.id.widgetRow3, "setBackgroundColor", android.graphics.Color.argb(120,0,255,0));
         remoteViews.setTextViewText(R.id.widgetRow4, sharedPreferences.getString("LESSONROW4", ""));
+        remoteViews.setInt(R.id.widgetRow4, "setBackgroundColor", android.graphics.Color.argb(120,255,255,0));
         remoteViews.setTextViewText(R.id.widgetRow5, sharedPreferences.getString("LESSONROW5", ""));
+        remoteViews.setInt(R.id.widgetRow5, "setBackgroundColor", android.graphics.Color.argb(120,255,0,255));
         remoteViews.setTextViewText(R.id.widgetRow6, sharedPreferences.getString("LESSONROW6", ""));
+        remoteViews.setInt(R.id.widgetRow6, "setBackgroundColor", android.graphics.Color.argb(120,0,255,255));
 
         //Aktualisierung des ButtonClickListeners
         remoteViews.setOnClickPendingIntent(R.id.refresh_button, WidgetMain.buildButtonPendingIntent(context));
